e528b24a6940177710a0e895f68467c8b8cd030a
Patrick Moeller <pmoeller91@gmail.com>
2024-04-29 15:01:25 -0500

    feat: reset button now resets optional contents
    When solving a puzzle it may make sense to provide exact values for
    the various non-mimic content types. However, these values change
    between puzzles almost every time. To provide a clean slate for a
    new puzzle, it makes sense to allow the reset button to reset
    these values as well.

--- src/components/reset-puzzle-button/ResetPuzzleButton.tsx
+++ src/components/reset-puzzle-button/ResetPuzzleButton.tsx
@@ -1,15 +1,18 @@
-import { forwardRef } from 'react';
-import { ConfirmButtonContainer } from '../confirm-button/ConfirmButtonContainer';
-import { useTranslation } from 'react-i18next';
-import { useSetAtom } from 'jotai';
-import { AllChests, allChestsCallbackAtom } from '@/atoms/allChestsAtom';
-import { useCallback } from 'use-memo-one';
-import { CHEST_CONTENTS } from '@/types/chestContents';
-import { CHEST_COLOR } from '@/types/chestColor';
-import { getDefaultHint } from '@/util/getDefaultHint';
-import { CHEST_HINT_TYPE } from '@/types/chestHint';
-import { selectedTabAtom } from '@/atoms/selectedTabAtom';
-import { MAIN_TAB } from '@/types/mainTab';
+import { forwardRef } from "react";
+import { ConfirmButtonContainer } from "../confirm-button/ConfirmButtonContainer";
+import { useTranslation } from "react-i18next";
+import { useSetAtom } from "jotai";
+import { AllChests, allChestsCallbackAtom } from "@/atoms/allChestsAtom";
+import { useCallback } from "use-memo-one";
+import { CHEST_CONTENTS } from "@/types/chestContents";
+import { CHEST_COLOR } from "@/types/chestColor";
+import { getDefaultHint } from "@/util/getDefaultHint";
+import { CHEST_HINT_TYPE } from "@/types/chestHint";
+import { selectedTabAtom } from "@/atoms/selectedTabAtom";
+import { MAIN_TAB } from "@/types/mainTab";
+import { numGoldFormValueAtom } from "@/atoms/numGoldFormValueAtom";
+import { numGearFormValueAtom } from "@/atoms/numGearFormValueAtom";
+import { numItemsFormValueAtom } from "@/atoms/numItemsFormValueAtom";
 
 interface ResetPuzzleButtonProps {
   className?: string;
@@ -18,13 +21,15 @@ interface ResetPuzzleButtonProps {
 const ResetPuzzleButton = forwardRef<HTMLButtonElement, ResetPuzzleButtonProps>(
   function ResetPuzzleButton({ className }, ref) {
     const { t } = useTranslation();
-    const resetButtonLabel = t('resetPuzzleButton.resetButtonLabel');
-    const confirmDialogTitle = t('resetPuzzleButton.confirmDialogTitle');
-    const confirmDialogExplanation = t(
-      'resetPuzzleButton.confirmDialogExplanation'
-    );
+    const resetButtonLabel = t("resetPuzzleButton.resetButtonLabel");
+    const confirmDialogTitle = t("resetPuzzleButton.confirmDialogTitle");
+    const confirmDialogExplanation = t("resetPuzzleButton.confirmDialogExplanation");
     const setAllChestsCallback = useSetAtom(allChestsCallbackAtom);
     const setSelectedTab = useSetAtom(selectedTabAtom);
+    const setNumGear = useSetAtom(numGearFormValueAtom);
+    const setNumGold = useSetAtom(numGoldFormValueAtom);
+    const setNumItems = useSetAtom(numItemsFormValueAtom);
+
     const handleOnConfirm = useCallback(() => {
       setAllChestsCallback(
         (allChests) =>
@@ -34,13 +39,16 @@ const ResetPuzzleButton = forwardRef<HTMLButtonElement, ResetPuzzleButtonProps>(
               color: CHEST_COLOR.red,
               contents: CHEST_CONTENTS.unknown,
               hint: getDefaultHint(CHEST_HINT_TYPE.selfAsleep),
-            }))
-          ) as AllChests
+            })),
+          ) as AllChests,
       );
+      setNumGear("", true);
+      setNumGold("", true);
+      setNumItems("", true);
       // If we're resetting we'd probably like to immediately go set up a new
       // puzzle to solve, so go ahead and move back to the game field
       setSelectedTab(MAIN_TAB.gameField);
-    }, [setAllChestsCallback, setSelectedTab]);
+    }, [setAllChestsCallback, setNumGear, setNumGold, setNumItems, setSelectedTab]);
     return (
       <ConfirmButtonContainer
         className={className}
@@ -51,7 +59,7 @@ const ResetPuzzleButton = forwardRef<HTMLButtonElement, ResetPuzzleButtonProps>(
         onConfirm={handleOnConfirm}
       />
     );
-  }
+  },
 );
 
 export { ResetPuzzleButton };
